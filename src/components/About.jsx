5208752b9bacbaace1933b23a3bc0c19a97810db
unknown <salma.baatout88@gmail.com>
2023-01-20 22:26:32 +0100

    About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div></div>
+    <div className='About'>
+      <div className='container'>
+      <div className='row'>
+
+        <div className='col-md-6'>
+      <h1 className='titleAbout'>Little Lemon</h1>
+      <p className='textAbout'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis at optio voluptatibus
+        tempore magnam quasi neque veniam molestias non sint? Eaque beatae commodi recusandae cum,
+            non alias quas voluptates nobis!</p>
+        </div>
+
+        <div className='col-md-6'>
+
+    <img src="images/Mario and Adrian A.jpg" class="overlay"/>
+    <img src="images/Mario and Adrian B.jpg" class="underlay"/>
+    </div>
+
+
+      </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -30,7 +30,7 @@ const Testimonials = () => {
                         <div class="tour-item ">
                             <div class="tour-desc bg-white">
                                 <div class="tour-text color-grey-3 text-center">&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur vero, repudiandae minima labore at eos laudantium omnis, numquam enim magni rem vitae.&rdquo;</div>
-                                <div class="d-flex justify-content-center pt-2 pb-2"><img class="tm-people" src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""></img></div>
+                                <div class="d-flex justify-content-center pt-2 pb-2"><img class="tm-people" src="https://images.pexels.com/photos/6625914/pexels-photo-6625914.jpeg" alt=""></img></div>
                                 <div class="link-name d-flex justify-content-center">Balbir Kaur</div>
                                 <div class="link-position d-flex justify-content-center">Client</div>
                             </div>
@@ -40,7 +40,7 @@ const Testimonials = () => {
                         <div class="tour-item ">
                             <div class="tour-desc bg-white">
                                 <div class="tour-text color-grey-3 text-center">&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur vero, repudiandae minima labore at eos laudantium omnis, numquam enim magni rem vitae.&rdquo;</div>
-                                <div class="d-flex justify-content-center pt-2 pb-2"><img class="tm-people" src="https://images.pexels.com/photos/4946604/pexels-photo-4946604.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""></img></div>
+                                <div class="d-flex justify-content-center pt-2 pb-2"><img class="tm-people" src="https://images.pexels.com/photos/6625914/pexels-photo-6625914.jpeg" alt=""></img></div>
                                 <div class="link-name d-flex justify-content-center">Balbir Kaur</div>
                                 <div class="link-position d-flex justify-content-center">Client</div>
                             </div>
